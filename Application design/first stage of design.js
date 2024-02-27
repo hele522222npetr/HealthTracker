2b8264649767f6ce464732876de4d45f081db2e7
Helen Peterson <161514367+hele522222npetr@users.noreply.github.com>
2024-02-27 21:28:25 +0200

    Create first stage of design.js

---
+++ Application design/first stage of design.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom/extend-expect'
+
+
+const nextConfig = {
+  compiler: {
+    emotion: true,
+    styledComponents: true
+  },
+
+
+  "main": "index.js",
+  "dependencies": {
+    "@google-cloud/functions-framework": "^3.1.2",
+    "@slack/webhook": "^6.1.0",
+    "body-parser": "^1.20.0",
+    "json5": "^2.2.1"
+
+
+    "event_info": {
+      "log_message": "string",
+      "request_method": "string",
+      "request_url": "string",
+      "referrer": "string",
+      "user_agent": "string",
+      "service": "string",
+      "version": "string",
+      "response_status": "string"
+    }
